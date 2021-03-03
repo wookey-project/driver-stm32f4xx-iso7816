4a1e7d8afa269fd23ad5ede0ad6763a27f7b683c
Philippe Thierry <phil@reseau-libre.net>
2021-03-03 16:50:31 +0100

    [update] initial framaC specs for upper lib, basics

--- api/libdrviso7816.h
+++ api/libdrviso7816.h
@@ -27,19 +27,46 @@ typedef enum {
 /* The SMARTCARD_CONTACT pin is at state high (pullup to Vcc) when no card is
  * not present, and at state low (linked to GND) when the card is inserted.
  */
+
+/*@
+  @ assigns \nothing;
+  @ ensures \result == 0 || \result == 1;
+  */
 uint8_t platform_is_smartcard_inserted(void);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_set_smartcard_rst(uint8_t val);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_set_smartcard_vcc(uint8_t val);
 
 
 
+/*@
+  @ assigns \nothing;
+  @ ensures \result >= 0;
+  */
 int platform_smartcard_early_init(drv7816_map_mode_t map_mode);
 
+/*@
+  @ assigns \nothing;
+  @ ensures \result == 0;
+  */
 int platform_smartcard_init(void);
 
 /* Adapt clocks depending on what has been received */
+
+/*@
+  @ requires \valid_read(etu);
+  @ requires \valid(frequency);
+  @ requires \separated(etu,frequency);
+  @ assigns *frequency;
+  @ ensures \result == 0 || \result == -1;
+  */
 int platform_SC_adapt_clocks(uint32_t *etu, uint32_t *frequency);
 
 /*
@@ -47,36 +74,87 @@ int platform_SC_adapt_clocks(uint32_t *etu, uint32_t *frequency);
  * bytes send and receive stuff here.
  */
 
+/*@
+  @ assigns \nothing;
+  @ ensures \result == 0;
+  */
 int platform_SC_set_direct_conv(void);
 
+/*@
+  @ assigns \nothing;
+  @ ensures \result == 0;
+  */
 int platform_SC_set_inverse_conv(void);
 
 
 /* Smartcard putc and getc handling errors, with timeout in milliseconds */
+
+/*@
+  @ assigns \nothing;
+  */
 void platform_SC_flush(void);
 
+/*@
+  @ assigns *c;
+  @ ensures \result == 0 || \result == -1;
+  */
+/*
+ * TODO: FramaC assigns *c only if non-NULL
+ */
 int platform_SC_getc(uint8_t *c, uint32_t timeout, uint8_t reset);
 
+
+/*@
+  @ assigns \nothing;
+  @ ensures \result == 0 || \result == -1;
+  */
 int platform_SC_putc(uint8_t c, uint32_t timeout, uint8_t reset);
 
 /* Get ticks/time in milliseconds */
+/*@
+  @ assigns \nothing;
+  */
 uint64_t platform_get_microseconds_ticks(void);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_SC_reinit_smartcard_contact(void);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_SC_reinit_iso7816(void);
 
 /* shut the smartcard LED in case of communication error */
+/*@
+  @ assigns \nothing;
+  */
 void platform_smartcard_lost(void);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_smartcard_reinit(void);
 
+/*@
+  @ assigns \nothing;
+  */
 void platform_smartcard_register_user_handler_action(void (*action)(void));
 
+/*@
+  @ assigns \nothing;
+  */
 int platform_smartcard_map(void);
 
+/*@
+  @ assigns \nothing;
+  */
 int platform_smartcard_unmap(void);
 
+/*@
+  @ assigns \nothing;
+  */
 int platform_smartcard_set_1ETU_guardtime(void);
 
 #endif /* __SMARTCARD_ISO7816_PLATFORM_H__ */
